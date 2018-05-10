2607eff2d4ee2632cc1932731dd2d6ded3b1a89b
Sascha Böck <wi16b142@technikum-wien.at>
2018-05-10 23:36:30 +0200

    training

---
+++ MSMQTraining1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Dispatcher;
+using System.Text;
+using System.Threading.Tasks;
+using System.Messaging;
+using Shared;
+
+namespace MSMQTraining1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageQueue mymsmq = new MessageQueue(@"FormatName:direct=os:.\private$\test1");
+            mymsmq.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
+            mymsmq.Send(new Message("MSMQ test"));
+
+        }
+    }
+}
---
+++ WebServiceTraining1Consumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using Shared;
+
+namespace WebServiceTraining1Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WCFServiceReference1.Service1Client wcfClient = new WCFServiceReference1.Service1Client();
+            WebServiceReferenceTraining1.WebService1SoapClient client = new WebServiceReferenceTraining1.WebService1SoapClient();
+            Console.WriteLine(client.HelloWorld());
+            Console.WriteLine(wcfClient.HelloWorld());
+
+
+            MessageQueue myreceivequeue = new MessageQueue(@"FormatName:direct=os:.\private$\test1");
+            myreceivequeue.Formatter = new XmlMessageFormatter(new Type[]{typeof(string)});
+            Message myMessage = myreceivequeue.Receive();
+            
+            Console.WriteLine((string)myMessage.Body.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
